8eb5c291ed26b20100a3b8e5c307e5a3fb3ff5ca
Alexandr_Shlyachkov <i904gcc@gmail.com>
2026-02-24 16:21:24 +0300

    const & iPoint

--- inc/iPoint.h
+++ inc/iPoint.h
@@ -3,26 +3,26 @@ namespace geo {
 using coord = double;
 
 using dim2d = struct {
-	coord x = {};
-	coord y = {};
+	coord x{};
+	coord y{};
 };
 using dim3d = struct {
-	coord x = {};
-	coord y = {};
-	coord z = {};
+	coord x{};
+	coord y{};
+	coord z{};
 };
 
 	class iPoint2d {
 	public:
 		virtual ~iPoint2d() = default;
 
-		virtual bool Move(const dim2d dim) = 0;
+		virtual bool Move(const dim2d& dim) = 0;
 	};
 
 	class iPoint3d {
 	public:
 		virtual ~iPoint3d() = default;
 
-		virtual bool Move(const dim3d dim) = 0;
+		virtual bool Move(const dim3d& dim) = 0;
 	};
 }
